36f82f56a3c9e356f269cd3cae8ae7d89d5b3556
Renee Huang <renee.v.huang@gmail.com>
2018-09-06 11:49:13 -0500

    connected successfully?

---
+++ LoginDemo/app/src/main/java/com/example/a1950082/logindemo/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.a1950082.logindemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+    private TextView Info;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText) findViewById(R.id.etName);
+        Password = (EditText) findViewById(R.id.etName);
+        Info = (TextView) findViewById(R.id.tvInfo);
+        Login = (Button) findViewById(R.id.btnLogin);
+    }
+
+    private void validate(String userName, String userPassword) {
+        if ((userName == "Admin") && (userPassword == "1234")) {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        }
+    }
+}
