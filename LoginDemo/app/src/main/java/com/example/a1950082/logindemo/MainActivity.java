d8b1c1725cf45f40bc9c924a8f491be8707369ac
Renee Huang <renee.v.huang@gmail.com>
2018-09-07 10:41:43 -0500

    finished tutorial

--- LoginDemo/app/src/main/java/com/example/a1950082/logindemo/MainActivity.java
+++ LoginDemo/app/src/main/java/com/example/a1950082/logindemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a1950082.logindemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText Password;
     private Button Login;
     private TextView Info;
-
+    private int counter = 5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +24,27 @@ public class MainActivity extends AppCompatActivity {
         Password = (EditText) findViewById(R.id.etName);
         Info = (TextView) findViewById(R.id.tvInfo);
         Login = (Button) findViewById(R.id.btnLogin);
-    }
+        Info.setText("No of attempts remaining: 5");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+        }
 
     private void validate(String userName, String userPassword) {
-        if ((userName == "Admin") && (userPassword == "1234")) {
+        if ((userName.equals("Admin")) && (userPassword.equals("1234"))) {
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+        else{
+            counter--;
+
+            Info.setText("No of attempts remaining: " + String.valueOf(counter));
+            if(counter == 0)
+                Login.setEnabled(false);
         }
     }
 }
